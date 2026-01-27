26f0257f02ccbd5f11c1efa5b257fa555487d9ea
Muhammed Wael <mw26200400@gmail.com>
2026-01-27 19:20:38 +0200

    Change server log message

--- main.js
+++ main.js
@@ -25,4 +25,4 @@ app.post('/send-otp', async (req, res) => {
   }
 });
 
-app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Server is running`));
